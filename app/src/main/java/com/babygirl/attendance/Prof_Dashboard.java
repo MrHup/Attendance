b734085d858fffa3bb50a2650c5217b7ce8ded63
MrHup <hup.hubspace@gmail.com>
2020-03-23 14:11:10 +0200

    added disconnect button to instructor dashboard

--- app/src/main/java/com/babygirl/attendance/Prof_Dashboard.java
+++ app/src/main/java/com/babygirl/attendance/Prof_Dashboard.java
@@ -1,13 +1,32 @@
 package com.babygirl.attendance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Prof_Dashboard extends AppCompatActivity {
+    public void logout(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // user is now signed out
+                        startActivity(new Intent(Prof_Dashboard.this, MainActivity.class));
+                        Log.d("debug_firebase","Logout succesfull");
+                        finish();
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,14 @@ public class Prof_Dashboard extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Prof_Dashboard.this, AddCourse.class));
+            }
+        });
+
+        ImageButton button_logout2 = findViewById(R.id.button_logout2);
+        button_logout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
             }
         });
     }
