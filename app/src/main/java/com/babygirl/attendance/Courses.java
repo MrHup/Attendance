e18523508cbd2c22e102b5546c2ccc5338907233
MrHup <hup.hubspace@gmail.com>
2020-04-03 12:25:58 +0300

    added examples for querring in firebase

--- app/src/main/java/com/babygirl/attendance/Courses.java
+++ app/src/main/java/com/babygirl/attendance/Courses.java
@@ -19,8 +19,24 @@ import android.widget.TextView;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+////--------------------------------------------------------------
+////-------------------DEBUG ACTIVITY-----------------------------
+////--------------------------------------------------------------
 
 public class Courses extends AppCompatActivity {
     TextView textView;
@@ -121,7 +137,79 @@ public class Courses extends AppCompatActivity {
             }
         });
 
-        // check if user is professor or teacher
+
+        // to test querries
+        Button test_querries_button = findViewById(R.id.test_querries_button);
+        test_querries_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("debug_querry","bruh1");
+                // testing with querries
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                // QUERRY EXAMPLE for retrieving all with child value equal to
+                Query query = FirebaseDatabase.getInstance().getReference().child("TestQuerry").orderByChild("val").equalTo("Bobby");
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.d("debug_querry","bruh2");
+                        Log.d("debug_querry",dataSnapshot.getValue() + "!");
+                        if (dataSnapshot.exists()) {
+                            Log.d("debug_querry","bruh3");
+                            int i = 0;
+                            for(DataSnapshot d : dataSnapshot.getChildren()) {
+                                Log.d("debug_querry",d.getKey() + " : " + d.getValue());
+                                i++;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                // GET REFERENCE EXAMPLE for retrieving all keys from a root
+                DatabaseReference myRef = database.getReference("Users").child("+8O4ukqAF8cMQyf1BUSeid1Tvugs2").child("interest_courses");
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            String key = child.getKey();
+                            Log.d("debug_querry",key);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
+                });
+
+                // QUERRY EXAMPLE for retrieving all keys from a root
+                Query query2 = FirebaseDatabase.getInstance().getReference().child("Users").child("+8O4ukqAF8cMQyf1BUSeid1Tvugs2").child("interest_courses");
+                query2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.d("debug_querry",dataSnapshot.getValue() + "!");
+                        if (dataSnapshot.exists()) {
+                            int i = 0;
+                            for(DataSnapshot d : dataSnapshot.getChildren()) {
+                                Log.d("debug_querry",d.getKey() + " : " + d.getValue());
+                                i++;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
 
 
     }
